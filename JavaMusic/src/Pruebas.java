60a2e2376ca61b9f5e77f5384801dc97161071b2
Moringos7 <moringos7@gmail.com>
2019-11-09 16:38:24 -0600

    Arreglo de Ventana

---
+++ JavaMusic/src/Pruebas.java
@@ -0,0 +1,26 @@
+
+public class Pruebas {
+	public static void main(String args[]) {
+		Reproductor rep = new Reproductor("barbiegirl");
+		rep.start();
+		try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+		rep.pause();
+		try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+		rep.play();
+		try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+		rep.stopMusic();
+		rep.play();
+	}
+}
--- JavaMusic/src/VentanaJavaMusic.java
+++ JavaMusic/src/VentanaJavaMusic.java
@@ -1,6 +1,5 @@
 import java.awt.BorderLayout;
 
-<<<<<<< HEAD
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class VentanaJavaMusic extends JFrame {
@@ -19,32 +18,5 @@ public class VentanaJavaMusic extends JFrame {
 	}
 	public static void main(String[] args) {
 		VentanaJavaMusic vjm= new VentanaJavaMusic();
-=======
-public class VentanaJavaMusic {
-
-	
-	public static void main(String args[]) {
-		Reproductor rep = new Reproductor("barbiegirl");
-		rep.start();
-		try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-		rep.pause();
-		try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-		rep.play();
-		try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-		rep.stopMusic();
-		rep.play();
->>>>>>> 5a8d39eb7290e7996433108f1689b99b23225a06
 	}
 }

902fef3fb4a1547aefaa2f7d51d2003a2d6a5a53
Diego Vazquez <a00226803@itesm.mx>
2019-11-09 14:51:35 -0600

    Nuevas clases

---
+++ JavaMusic/src/JavaMusic.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JavaMusic {
+	private Playlist listaFavoritos;
+	private Playlist listaNegados;
+	//private Reproductor reproductor;
+	private Hashtable<String,Artista>artistas;
+	private Hashtable<String,Album>albumes;
+	private Hashtable<String, Genero>generos;
+	
+	public JavaMusic() {
+		try {
+			BufferedReader bf=new BufferedReader(new FileReader("playlist.txt"));
+			while(bf.readLine()!=null) {
+				while(!bf.readLine().equals("-")) {
+					//this.listaFavoritos.canciones.add()
+				}
+			}
+		}catch(FileNotFoundException ex) {
+			System.out.println("No se encontro el archivo");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
--- JavaMusic/src/VentanaJavaMusic.java
+++ JavaMusic/src/VentanaJavaMusic.java
@@ -1,4 +1,22 @@
+import java.awt.BorderLayout;
 
-public class VentanaJavaMusic {
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class VentanaJavaMusic extends JFrame {
+	private JPanel panelGeneral;
+	private JavaMusic app;
+	private PanelOpciones po;
+	
+	public VentanaJavaMusic() {
+		super("JavaMusic");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(1000,650);
+		this.panelGeneral=new PanelInicio();
+		this.po=new PanelOpciones();
+		this.add(po, BorderLayout.WEST);
+		this.setVisible(true);
+	}
+	public static void main(String[] args) {
+		VentanaJavaMusic vjm= new VentanaJavaMusic();
+	}
 }
